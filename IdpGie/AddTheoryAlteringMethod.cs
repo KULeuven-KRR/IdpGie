80713e228fc4aee61a7ae3534356d050ea972ad0
KommuSoft <vanonsem.willem@gmail.com>
2013-12-27 18:44:19 +0100

    more on theory altering

---
+++ IdpGie/AddTheoryAlteringMethod.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace IdpGie {
+	public class AddTheoryAlteringMethod : ITheoryAlteringMethod {
+		public AddTheoryAlteringMethod (ITerm term) {
+		}
+	}
+}
+
---
+++ IdpGie/ITheoryAlteringMethod.cs
@@ -0,0 +1,8 @@
+using System;
+using System.IO;
+
+namespace IdpGie {
+	public interface ITheoryAlteringMethod : IAlteringStreamMethod {
+	}
+}
+
