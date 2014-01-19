33ea0ae6d7cfea770b8211d82181bd96451d46f2
KommuSoft <vanonsem.willem@gmail.com>
2014-01-19 15:29:43 +0100

    towards more hooks

--- IdpGie/HookMapping.cs
+++ IdpGie/HookMapping.cs
@@ -5,9 +5,14 @@ using Gdk;
 namespace IdpGie {
 	[Mapper]
 	public static class HookMapping {
-		[HookMethod ("keydown", 1000.0d, TermType.Named)]
-		public static IHook KeyDown (DrawTheory dt, IFunctionInstance key, IEnumerable<IAtom> body) {
-			return new KeyHook (body, Key.A);
+		[HookMethod ("keydown", 1000.0d, TermType.String)]
+		public static IHook KeyDown (DrawTheory dt, string key, IEnumerable<IAtom> body) {
+			return new KeyHook (body, key);
+		}
+
+		[HookMethod ("mousedownregion", 1000.0d, TermType.Float, TermType.Float, TermType.Float, TermType.Float)]
+		public static IHook KeyDown (DrawTheory dt, string key, IEnumerable<IAtom> body) {
+			return new KeyHook (body, key);
 		}
 	}
 }
