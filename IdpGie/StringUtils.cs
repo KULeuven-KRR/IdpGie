9a9b65b1fc9baf38d152df04e720e8d70b9b11c9
KommuSoft <vanonsem.willem@gmail.com>
2014-01-22 00:40:07 +0100

    string replacements

--- IdpGie/StringUtils.cs
+++ IdpGie/StringUtils.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace IdpGie.Utils {
 	public static class StringUtils {
@@ -10,16 +11,36 @@ namespace IdpGie.Utils {
 			}
 		}
 
-		public static string AlwaysEffective (string x) {
-			return EffectiveOrDefault (x);
-		}
-
 		public static string EffectiveOrDefault (string x, string dflt = "") {
 			if (x == null) {
 				return dflt;
 			} else {
 				return x;
 			}
+		}
+
+		public static string EffectiveOrDefault (ref string target, string x) {
+			EffectiveOrDefault (ref target, x, target);
+		}
+
+		public static string EffectiveOrDefault (ref string target, string x, string dflt) {
+			if (x != null) {
+				target = x;
+			} else {
+				target = dflt;
+			}
+		}
+
+		public static string ReplaceDollar (string source, params Tuple<string,string>[] tuples) {
+			return ReplaceDollar (source, (IEnumerable<Tuple<string,string>>)tuples);
+		}
+
+		public static string ReplaceDollar (string source, IEnumerable<Tuple<string,string>> tuples) {
+			string copy = source;
+			foreach (Tuple<string,string> tuple in tuples) {
+				copy = copy.Replace (string.Format (@"$({0})", tuple.Item1), tuple.Item2);
+			}
+			return copy;
 		}
 	}
 }
