0e93ae8cec96c8e2cd0119644a61b59b40ff4a29
KommuSoft <vanonsem.willem@gmail.com>
2014-01-26 15:21:14 +0100

    bugfix

--- IdpGie/OutputDeviceAttribute.cs
+++ IdpGie/OutputDeviceAttribute.cs
@@ -2,32 +2,11 @@ using System;
 
 namespace IdpGie {
 	[AttributeUsage (AttributeTargets.Class)]
-	public class OutputDeviceAttribute : Attribute, INameDescription {
-		private readonly string name, description;
-
-		#region IName implementation
-
-		public string Name {
-			get {
-				return this.name;
-			}
+	public class OutputDeviceAttribute : NamedDescribedAttributeBase {
+		public OutputDeviceAttribute (string name) : base (name) {
 		}
 
-		#endregion
-
-		#region IDescription implementation
-
-		public string Description {
-			get {
-				return description;
-			}
-		}
-
-		#endregion
-
-		public OutputDeviceAttribute (string name, string description = "No description available") {
-			this.name = name;
-			this.description = description;
+		public OutputDeviceAttribute (string name, string description) : base (name, description) {
 		}
 	}
 }
