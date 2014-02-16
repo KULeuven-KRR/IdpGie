7c41309a6237f7b174d35f38976ce77ac9062c75
KommuSoft <vanonsem.willem@gmail.com>
2014-02-16 23:10:11 +0100

    start implementing listener

--- IdpGie/OutputHttpServerDevice.cs
+++ IdpGie/OutputHttpServerDevice.cs
@@ -1,9 +1,14 @@
 using System;
 using System.Web;
+using System.IO;
+using System.Net.Sockets;
 
 namespace IdpGie {
 	[OutputDevice ("httpserver", "A HTTP server that runs on a specified port and handle user request using AJAX.")]
 	public class OutputHttpServerDevice : OutputDevice, IHttpGieServer {
+		protected int port;
+		TcpListener listener;
+
 		public OutputHttpServerDevice (DrawTheory dt) : base (dt) {
 		}
 
@@ -18,7 +23,17 @@ namespace IdpGie {
 		#region IHttpHandler implementation
 
 		public void ProcessRequest (HttpContext context) {
-			throw new NotImplementedException ();
+			HttpRequest rq = context.Request;
+			HttpResponse rp = context.Response;
+			rp.StatusCode = 200;
+			StreamWriter sw = new StreamWriter (rp.OutputStream);
+			sw.WriteLine ("<html><body><h1>test server</h1>");
+			sw.WriteLine ("Current Time: " + DateTime.Now.ToString ());
+			sw.WriteLine ("url : {0}", rq.Url);
+			sw.WriteLine ("<form method=post action=/form>");
+			sw.WriteLine ("<input type=text name=foo value=foovalue>");
+			sw.WriteLine ("<input type=submit name=bar value=barvalue>");
+			sw.WriteLine ("</form>");
 		}
 
 		public bool IsReusable {
